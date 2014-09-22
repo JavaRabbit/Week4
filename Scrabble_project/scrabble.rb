e50f0f64559c282001397a51a2e53971ec2eadbc
Bonnie <kbonnie@gmail.com>
2014-09-22 15:00:11 -0700

    adding Scrabble project

---
+++ Scrabble_project/scrabble.rb
@@ -0,0 +1,6 @@
+# scrabble.rb  TDD project
+# Sept. 22, 2014
+
+class Scrabble # this makes the line 'describe Scrabble do' happy!
+
+end
---
+++ Scrabble_project/spec/scrabble_spec.rb
@@ -0,0 +1,5 @@
+require './scrabble'
+
+describe Scrabble do #this probably is asking for class named Scrabble in scrabble.rb
+
+end
