99c1fe9889cca11d0062f73b19b114179264ad05
Bonnie <kbonnie@gmail.com>
2014-09-22 15:13:53 -0700

    added first test

--- Scrabble_project/scrabble.rb
+++ Scrabble_project/scrabble.rb
@@ -3,4 +3,12 @@
 
 class Scrabble # this makes the line 'describe Scrabble do' happy!
 
+  def initialize(n)
+    @n = n
+  end
+
+  def method1
+    1 # this returns 1 and makes the 
+  end
+
 end
--- Scrabble_project/spec/scrabble_spec.rb
+++ Scrabble_project/spec/scrabble_spec.rb
@@ -2,4 +2,11 @@ require './scrabble'
 
 describe Scrabble do #this probably is asking for class named Scrabble in scrabble.rb
 
+
+  describe "first test" do
+    it "returns 1" do
+      expect(Scrabble.new(1).method1).to eq 1
+    end
+  end
+
 end
